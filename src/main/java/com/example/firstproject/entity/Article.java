4106cbd547d40d825b0fcba96a5606e78957f072
정승민 <ppk052@naver.com>
2024-10-31 13:58:57 +0900

    11강완료

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity //DB가 해당 객체를 인식 가능
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Article {
 
     @Id
@@ -18,22 +24,4 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
-
-    public Article() {
-
-    }
 }
